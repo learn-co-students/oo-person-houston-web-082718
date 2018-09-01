580a8c4677d292f124e36cfe1a1fa526df4973b0
Naeblis <johnmlowery@gmail.com>
2018-09-01 18:26:42 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,92 +1,83 @@
-# your code goes here
 require 'pry'
 
-class Person
-  attr_accessor :bank_account
-  attr_reader :name, :happiness, :hygiene
 
- def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
-   @name = name
-   @bank_account = bank_account
-   @happiness = happiness
-   @hygiene = hygiene
- end
-  def happiness=(happiness)
-    if happiness > 10
-      @happiness = 10
-    elsif happiness < 0
-      @happiness = 0
-    else
-      @happiness = happiness
-    end
+class Person
+
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+  def initialize(name, bank_account=25, happiness=8, hygiene=8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+  def happiness=(n)
+     @happiness = n
+     @happiness = 10 if @happiness > 10
+     @happiness = 0 if @happiness < 0
   end
 
   def hygiene=(n)
-    if n > 10
-      @hygiene = 10
-    elsif n < 0
-      @hygiene = 0
-    else
     @hygiene = n
-    end
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
   end
+
   def happy?
-    if happiness > 7
-      return true
-    else
-      return false
+    if @happiness > 7
+      true
+    else 
+      false
     end
   end
+
   def clean?
-    if hygiene > 7
-      return true
-    else false
+    if @hygiene > 7
+      true
+    else 
+      false
     end
+  end
+
+  def get_paid(amount)
+    @bank_account = @bank_account += amount
+    "all about the benjamins"
   end
 
   def take_bath
-      self.hygiene += 4
-      return "♪ Rub-a-dub just relaxing in the tub ♫"
-    end
-
-  def get_paid(salary)
-    @bank_account += salary
-    return 'all about the benjamins'
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
-    self.hygiene -=  3
+    self.hygiene -= 3
     self.happiness += 2
-    return "♪ another one bites the dust ♫"
+    "♪ another one bites the dust ♫"
   end
-  def call_friend(friend)
+
+  def call_friend(person)
     self.happiness += 3
-    friend.happiness += 3
-    return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    person.happiness += 3
+
+    "Hi #{person.name}! It's #{self.name}. How are you?"
   end
 
   def start_conversation(person, topic)
     if topic == 'politics'
+      self.happiness -= 2
       person.happiness -= 2
-      self.happiness -=2
-      "blah blah partisan blah lobbyist"
+      'blah blah partisan blah lobbyist'
     elsif topic == 'weather'
-      person.happiness += 1
       self.happiness += 1
+      person.happiness += 1
       'blah blah sun blah rain'
-    else
-      "blah blah blah blah blah"
-      end
+    else topic == 'other'
+      'blah blah blah blah blah'
     end
 
+  end
 
 
-
-
-
-
-
-
-
-
+  
 end
